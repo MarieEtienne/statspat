af36f1423d045ee1f77fca336586cea9eaebff0e
Marie-Pierre Etienne <marie-pierre.etienne@ensai.fr>
2025-02-03 12:02:34 +0100

    Fix erros in TD swiss

--- pluies.R
+++ pluies.R
@@ -4,21 +4,21 @@ library(gstat)
 library(sf)
 
 
-dta_full_sf <- st_read(dsn = "swiss_full/swiss_rain_full.shp") 
+dta_full_sf <- st_read(dsn = "swiss/swiss_rain_full.shp") 
 dta_sf <- st_read(dsn = "swiss/swiss_rain.shp")
 bords_sf <- st_read(dsn = "swiss/bords.shp") 
 elev_sf <- st_read(dsn = "swiss/elev.shp") 
 
 dta_sf |> st_coordinates() |> bind_cols(dta_sf) |> ggplot() + 
     geom_sf(data = bords_sf, linewidth = 0.1) +
-    geom_circle(aes(x0=X, y0=Y, r=pluies/100, fill = pluies)) +
+    geom_circle(aes(x0=X, y0=Y, r=pluies*10, fill = pluies)) +
     scale_fill_viridis_c(name = "Pluies", option = "mako", direction = -1) 
 
 
 dta_full_sf |> st_coordinates() |> bind_cols(dta_full_sf) |> 
     ggplot() + 
     geom_sf(data = bords_sf, linewidth = 0.1) +
-    geom_circle(aes(x0=X, y0=Y, r=pluies/100, fill = pluies)) +
+    geom_circle(aes(x0=X, y0=Y, r=pluies*10, fill = pluies)) +
     scale_fill_viridis_c(name = "Pluies", option = "mako", direction = -1) 
 
 elev_sf |> ggplot() + 
@@ -56,7 +56,7 @@ nuees_dta |> st_drop_geometry() |> ggplot()  + geom_point(aes(x=h, y = ecart2)) 
 # 1. Variogramme empirique
 #-------------------------
 
-m.d = 120
+m.d = 120000
 vario.cloud = variogram(pluies~1, data = dta_sf, cloud = TRUE)
 vario.cloud |> ggplot(aes(x=dist, y =gamma)) + geom_point() + ggtitle("Nuée variographique") 
 
@@ -69,7 +69,7 @@ ggplot(data = vario.b, aes(x=dist, y =gamma)) + facet_wrap(~dir.hor) + geom_poin
 
 ## choix d'un modèle exponentiel
 vario.iso = variogram(pluies~1, data = dta_sf)
-v.fit = fit.variogram(vario.iso, vgm(model =  "Exp", 15000, 60))
+v.fit = fit.variogram(vario.iso, vgm(model =  "Exp", 15000, 600))
 vario_fit_exp <- variogramLine(v.fit, maxdist = m.d)
 
 ggplot(data = vario.iso, aes(x=dist, y =gamma))  + geom_point() + 
@@ -79,30 +79,38 @@ ggplot(data = vario.iso, aes(x=dist, y =gamma))  + geom_point() +
 # 3. Krigeage
 #------------
 ## transform in sf object
-dta_full_sf <- st_as_sf(dta_full_sf)
 Kfull <- krige(formula = pluies ~ 1, locations = dta_sf, 
                newdata = dta_full_sf,  model = v.fit)
 Kfull |> st_join(dta_full_sf) |> 
-    mutate( error = pluies- var1.pred) |> ggplot() + geom_sf(aes(col = error)) +
+    mutate( error = pluies- var1.pred) |> 
+    ggplot() + geom_sf(aes(col = error), size =4) +
     theme_minimal() +
-    theme(legend.position = "right") +
-    scale_color_viridis_c(name = "Pluies", option = "plasma")
+    theme(legend.position = "right")  +
+    scale_color_viridis_c(name = "Erreur", option = "plasma")
+
 RMSEP = Kfull |> st_join(dta_full_sf) |>  mutate( error2 = (pluies- var1.pred)^2) |> 
-    summarise(rmsep = sum(error2)) |> pull(rmsep)
+    summarise(rmsep = sqrt(sum(error2)/n())) |> pull(rmsep)
 
 # 4. Krigeage sur une grille
-#------------
+#------------ 
+
 grid <- st_make_grid(bords_sf, n= c(50, 50))
-dta |> ggplot()  +
+grid |>  st_intersection(bords_sf) |> ggplot() + geom_sf() 
+
+grid |> ggplot() + geom_sf()
+
+dta_sf |>  st_coordinates() |> bind_cols(dta_sf)  |> ggplot()  +
     scale_fill_viridis_c(name = "Pluies", option = "mako", direction = -1)  +
+    geom_sf(data = bords_sf, linewidth = 0.1) +
     geom_sf(data= grid) +
-    geom_path(data = bords, aes(x=x, y=y))+ 
-    geom_circle(aes(x0=x, y0=y, r=pluies/100, fill = pluies))
+    geom_circle(aes(x0=X, y0=Y, r=pluies*10, fill = pluies))
+
+
 Kfull <- krige(formula = pluies ~ 1, locations = dta_sf, newdata = grid,  model = v.fit)
 
 
 Kfull |> ggplot() + geom_sf(aes(fill = var1.pred)) + 
-    geom_sf(data = dta_sf, aes(col = pluies)) +
+    geom_sf(data = dta_sf, aes(col = pluies), size = 3) +
     scale_color_viridis_c(name = "Z", option = "mako", transform= "log10")+
     scale_fill_viridis_c( option = "mako", transform= "log10") 
 
@@ -112,13 +120,6 @@ Kfull |> ggplot() +
     geom_sf(data = dta_sf, col = "red") +
     scale_color_viridis_c( option = "mako", transform= "log10") 
 
-dta_sf <- res
-res <- do.call('rbind', 
-               lapply(
-                   split(dta_sf, 1:nrow(dta_sf)),
-                   function(x) {
-                       st_join(x, elev_sf, join = st_nearest_feature)
-                   }))
 
 # 5. A votre tour
 #------------
